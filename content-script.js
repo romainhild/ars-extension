27bde91fab811baa5c67b5271941f2f0ba774a7d
romainhild <romain.hild@me.com>
2022-06-25 17:01:12 +0200

    feat: remove side video on all arstechnica pages

---
+++ content-script.js
@@ -0,0 +1,6 @@
+setTimeout(function() {
+    var containers = document.getElementsByClassName("cne-interlude-player-container");
+    if(containers.length > 0) {
+        containers[0].remove();
+    }
+}, 2000);
